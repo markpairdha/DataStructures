81a215525ffcb00bc67c1010aec83e7400e9b1fd
Mark  Sathish Pairdha <pms10@iitbbs.ac.in>
2019-05-10 00:15:56 +0530

    https://www.geeksforgeeks.org/find-length-of-a-linked-list-iterative-and-recursive/
    https://www.geeksforgeeks.org/find-length-of-a-linked-list-iterative-and-recursive/

---
+++ 2 Linked Lists/01.Single Linked List/03.LengthofLL.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+	public :
+		int data;
+		Node *next;
+};
+
+
+void push(Node **head,int data)
+{
+	Node *newnode = new Node();
+	newnode->data = data;
+	newnode->next = (*head);
+	*head = new_node;
+}
+
+int getCount(Node *head)
+{
+  if(head == NULL)
+     return 0;
+  return 1+getCount(head->next); 	
+}
+
+int main()
+{
+	Node *head = NULL;
+	push(&head, 1); 
+    push(&head, 3); 
+    push(&head, 1); 
+    push(&head, 2); 
+    push(&head, 1);
+    printf("count of nodes is %d",getCount(head));
+    return 0;
+}
