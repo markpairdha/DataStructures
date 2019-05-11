64c730573dcfb6d2fbdd68b4e0e87f25c950b5f5
Mark  Sathish Pairdha <pms10@iitbbs.ac.in>
2019-05-11 14:33:46 +0530

    https://www.geeksforgeeks.org/write-a-c-function-to-print-the-middle-of-the-linked-list/
    https://www.geeksforgeeks.org/write-a-c-function-to-print-the-middle-of-the-linked-list/

---
+++ 2 Linked Lists/01.Single Linked List/05.middleelement.cpp
@@ -0,0 +1,64 @@
+#include<stdio.h> 
+#include<stdlib.h> 
+  
+/* Link list node */
+struct node 
+{ 
+    int data; 
+    struct node* next; 
+}; 
+
+void printMiddle(struct node *head)
+{
+	struct node *one = head,*two = head;
+	while(two != NULL && two->next != NULL)
+	{
+		one = one->next;
+		two = two->next->next;
+	}
+	cout << one->data << "\n";
+}
+
+void push(struct node** head_ref, int new_data) 
+{ 
+    /* allocate node */
+    struct node* new_node = 
+        (struct node*) malloc(sizeof(struct node)); 
+  
+    /* put in the data  */
+    new_node->data  = new_data; 
+  
+    /* link the old list off the new node */
+    new_node->next = (*head_ref); 
+  
+    /* move the head to point to the new node */
+    (*head_ref)    = new_node; 
+} 
+  
+// A utility function to print a given linked list 
+void printList(struct node *ptr) 
+{ 
+    while (ptr != NULL) 
+    { 
+        printf("%d->", ptr->data); 
+        ptr = ptr->next; 
+    } 
+    printf("NULL\n"); 
+} 
+  
+/* Drier program to test above function*/
+int main() 
+{ 
+    /* Start with the empty list */
+    struct node* head = NULL; 
+    int i; 
+  
+    for (i=5; i>0; i--) 
+    { 
+        push(&head, i); 
+        printList(head); 
+        printMiddle(head); 
+    } 
+  
+    return 0; 
+}
